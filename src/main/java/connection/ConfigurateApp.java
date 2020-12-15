72fb9a5d8b08c52561879e8105cb9c6b0458e4d0
Krassilnikov-AV <kavkras83@gmail.com>
2020-12-15 23:02:10 +0300

    refactoring: configurate

--- src/main/java/connection/ConfigurateApp.java
+++ src/main/java/connection/ConfigurateApp.java
@@ -5,89 +5,85 @@ import java.io.IOException;
 import java.util.Properties;
 
 /**
- *
  * @author Aleks
  */
 public class ConfigurateApp {
 
-    private String path;         // путь для сохранения на сервере
-    private String databaseDriver;
-    private String databaseHost;
-    private String databasePort;
-    private String databaseName;
-    private String databaseUser;
-    private String databasePassword;
-    private String databaseUrl;
+	private String path;         // путь для сохранения на сервере
+	private String databaseDriver;
+	private String databaseHost;
+	private String databasePort;
+	private String databaseName;
+	private String databaseUser;
+	private String databasePassword;
+	private String databaseUrl;
 
-    private void init() throws IOException {
+	private void init() throws IOException {
 
-        Properties ps = new Properties();
+		Properties ps = new Properties();
 
-        try (FileInputStream fs = new FileInputStream("config.properties")) {
-            ps.load(fs);
+		try (FileInputStream fs = new FileInputStream("config.properties")) {
+			ps.load(fs);
 
-            if (ps.getProperty("database.host").length() > 0) {
-                databaseHost = ps.getProperty("database.host");
-            }
-            if (ps.getProperty("database.driver").length() > 0) {
-                databaseDriver = ps.getProperty("database.driver");
-            }
-            if (ps.getProperty("database.port").length() > 0) {
-                databasePort = ps.getProperty("database.port");
-            }
-            if (ps.getProperty("database.name").length() > 0) {
-                databaseName = ps.getProperty("database.name");
-            }
-            if (ps.getProperty("database.user").length() > 0) {
-                databaseUser = ps.getProperty("database.user");
-            }
-            if (ps.getProperty("database.password").length() > 0) {
-                databasePassword = ps.getProperty("database.password");
-            }
-            if (ps.getProperty("pathStorage").length() > 0) {
-                path = ps.getProperty("pathStorage");
-            }
-            if (ps.getProperty("database.url").length() > 0) {
-                databaseUrl = ps.getProperty("database.url");
-            }
-        }
-    }
+			if (ps.getProperty("database.host").length() > 0) {
+				databaseHost = ps.getProperty("database.host");
+			}
+			if (ps.getProperty("database.driver").length() > 0) {
+				databaseDriver = ps.getProperty("database.driver");
+			}
+			if (ps.getProperty("database.port").length() > 0) {
+				databasePort = ps.getProperty("database.port");
+			}
+			if (ps.getProperty("database.name").length() > 0) {
+				databaseName = ps.getProperty("database.name");
+			}
+			if (ps.getProperty("database.user").length() > 0) {
+				databaseUser = ps.getProperty("database.user");
+			}
+			if (ps.getProperty("database.password").length() > 0) {
+				databasePassword = ps.getProperty("database.password");
+			}
+			if (ps.getProperty("pathStorage").length() > 0) {
+				path = ps.getProperty("pathStorage");
+			}
+		}
+	}
 
-    public String getPath() {
-        return path;
-    }
+	public String getPath() {
+		return path;
+	}
 
-    public String getDatabaseUrl() {
-        return databaseUrl;
-    }
+	public String getDatabaseUrl() {
+		return databaseUrl =
+			databaseDriver + "://" + databaseHost + ":" + databasePort + "/" + databaseName;
+	}
+//
+//    public String getDatabaseDriver() {
+//        return databaseDriver;
+//    }
+//
+//    public String getDatabaseHost() {
+//        return databaseHost;
+//    }
+//
+//    public String getDatabasePort() {
+//        return databasePort;
+//    }
+//
+//    public String getDatabaseName() {
+//        return databaseName;
+//    }
 
-    public String getDatabaseDriver() {
-        return databaseDriver;
-    }
+	public String getDatabaseUser() {
+		return databaseUser;
+	}
 
-    public String getDatabaseHost() {
-        return databaseHost;
-    }
+	public String getDatabasePassword() {
+		return databasePassword;
+	}
 
-    public String getDatabasePort() {
-        return databasePort;
-    }
-
-    public String getDatabaseName() {
-        return databaseName;
-    }
-
-    public String getDatabaseUser() {
-        return databaseUser;
-    }
-
-    public String getDatabasePassword() {
-        return databasePassword;
-    }
-
-    @Override
-    public String toString() {
-        return path;
-    }   
-   
+	@Override
+	public String toString() {
+		return databaseUrl = databaseDriver + databaseHost + databaseName;
+	}
 }
