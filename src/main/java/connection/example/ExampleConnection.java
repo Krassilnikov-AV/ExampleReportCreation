519ab97895f9b30edc3e58909851e9efcdccd822
Krassilnikov-AV <kavkras83@gmail.com>
2020-12-15 22:47:06 +0300

    refactoring: exampleConnection

--- src/main/java/connection/example/ExampleConnection.java
+++ src/main/java/connection/example/ExampleConnection.java
@@ -7,6 +7,7 @@ package connection.example;
 import connection.ConfigurateApp;
 import doc.ReadExcelData;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.Date;
 import java.util.List;
@@ -15,54 +16,64 @@ import java.util.List;
  * Класс ExampleConnection
  */
 public class ExampleConnection {
-
 	private static Connection conn = null;
-	static PreparedStatement stm = null;
-	static ResultSet result = null;
+
 	static String SQL = "";
+	static PreparedStatement stm = null;
 
 //	private static String URL = "jdbc:postgresql://localhost:5432/test";
 //	private static String USERNAME = "postgres";
 //	private static String PASSWORD = "alex159";
 
-	ConfigurateApp conf = new ConfigurateApp();
-
-
 	public static void main(String[] args) throws SQLException {
+		insertQuerySQL();
+		viewData();
+	}
+
+	// метод для установления соединения
+	private static Connection getPostConnection() throws SQLException {
+		ConfigurateApp conf = new ConfigurateApp();
+		System.out.println("Устанавливаем соединение с БД...");
+
+		return DriverManager.getConnection(conf.getDatabaseUrl(), conf.getDatabaseUser(), conf.getDatabasePassword());
+	}
+
+	// метод для внесения данных в таблицу
+	private static void insertQuerySQL() throws SQLException {
 		//запрос на внесение прочитанных данных в столбец
-		SQL = "insert into contact(value) values (?)";
+		SQL = "insert into contact(person) values (?)";
 
 		try {
-			System.out.println("Устанавливаем соединение с БД...");
-			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
-			stm = conn.prepareStatement(SQL);
-
+			stm = getPostConnection().prepareStatement(SQL);
 			ReadExcelData read = new ReadExcelData();
-			List<<Integer><Integer><String><Date><>> list = read.getDataStringIntegerDate(0);
+			List<String> list = read.getDataStringIntegerDate(0);
 
 			for (String value : list) {
 				stm.setString(1, value);
+				stm.addBatch();
+				stm.executeUpdate();
 			}
-			stm.addBatch();
-			stm.executeUpdate();
-
-			// запрос просмотра данных в столбце
-			SQL = "SELECT value FROM \"public\".contact LIMIT 100000;";
-			stm = conn.prepareStatement(SQL);
-			result = stm.executeQuery();
-
-			while (result.next()) {
-				String value = result.getString("value");
-				System.out.println(value);
-			}
-		}
-	 catch(Exception e)	{
-		assert e != null; e.printStackTrace();
-	}
-		finally  {
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
 			conn.close();
 			System.out.println("Закрыли соединение с БД после внесения и просмотра данных...");
+		}
+	}
+
+	// метод для просмотра внесённых данных в таблицу
+	private static void viewData() throws SQLException {
+		ResultSet result = null;
+
+		// запрос просмотра данных в столбце
+		SQL = "SELECT contact FROM raspisanie";
+		stm = conn.prepareStatement(SQL);
+
+		result = stm.executeQuery();
+
+		while (result.next()) {
+			String value = result.getString("value");
+			System.out.println(value);
 		}
 	}
 }
