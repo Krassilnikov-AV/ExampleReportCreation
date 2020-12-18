56d70c049137fef6c999fb1630f113fdadb03b2a
Krassilnikov-AV <kavkras83@gmail.com>
2020-12-18 21:16:19 +0300

    refactoring: пробы вставки данных с её просмотром и модификация кода

--- src/main/java/connection/example/ExampleConnection.java
+++ src/main/java/connection/example/ExampleConnection.java
@@ -27,15 +27,13 @@ public class ExampleConnection {
 		conf.init();
 		System.out.println(conf.getDatabaseName());
 		System.out.println(getURL());
+//		getPostConnection();
+		insertQuerySQL();
+//		viewData();
 	}
 
 	/*
-	* private static String getUrl() {
-
-	//jdbc:derby://localhost:1527/Samples
-	String url = "jdbc:derby://localhost:1527/Samples";
-        System.out.println("есть url...");
-        return url;
+	метод, который возвращает url базы данных, прописанной в property файле
 }*/
 	private static String getURL() {
 		String databaseDriver = conf.getDatabaseDriver();
@@ -53,22 +51,22 @@ public class ExampleConnection {
 		final Connection connection;
 		connection = DriverManager.getConnection(getURL(), conf.getDatabaseUser(), conf.getDatabasePassword());
 		return connection;
-	//	return DriverManager.getConnection("jdbc:postgresql://localhost:5432/raspisanie", "postgres", "alex159");
 	}
 
-	// метод для внесения данных в таблицу
+	/*
+	    метод для внесения данных в таблицу выбранных с таблицы Excel
+	 */
 	public static void insertQuerySQL() throws SQLException {
 		//запрос на внесение прочитанных данных в столбец
-		SQL = "insert into contact(person) values (?)";
+		String SQLinsert = "insert into contact(person) values (?)";
 		try {
-			Connection connection = null;
-			//	Connection connection = getPostConnection();
-			//			stm = connection.prepareStatement(SQL);
+			Connection connection = getPostConnection();
+			stm = connection.prepareStatement(SQLinsert);
 			ReadExcelData read = new ReadExcelData();
-			List<String> list = read.getDataStringIntegerDate(0);
+			List<String> list = read.getDataStringIntegerDate(4);
 
 			for (String value : list) {
-				stm.setString(1, value);
+				stm.setString(0, value);
 				stm.addBatch();
 				stm.executeUpdate();
 			}
@@ -80,7 +78,9 @@ public class ExampleConnection {
 		}
 	}
 
-	// метод для просмотра внесённых данных в таблицу
+	/*
+	метод для просмотра внесённых данных в таблицу
+	*/
 	private static void viewData() throws SQLException {
 		ResultSet result = null;
 
@@ -88,9 +88,7 @@ public class ExampleConnection {
 		SQL = "SELECT contact FROM raspisanie";
 		try {
 			stm = conn.prepareStatement(SQL);
-
 			result = stm.executeQuery();
-
 			while (result.next()) {
 				String value = result.getString("value");
 				System.out.println(value);
@@ -102,4 +100,8 @@ public class ExampleConnection {
 			System.out.println("Закрыли соединение с БД после просмотра данных...");
 		}
 	}
+	/*
+	метод для удаления внесённых данных в таблицу
+	*/
+
 }
